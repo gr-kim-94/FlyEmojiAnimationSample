401d055adcb3203abce220f6bf640c3e58eb748e
김가람 <grkim>
2022-07-26 11:22:36 +0900

    - Lottie AnimationView Play - UIView Add Fly Curved Animation

--- FlyEmojiAnimationSample/AnimationViewController.swift
+++ FlyEmojiAnimationSample/AnimationViewController.swift
@@ -26,5 +26,50 @@ class AnimationViewController: UIViewController {
     }
     
     @objc func showAnimation() {
+        (0...8).forEach { _ in
+            addAnimationView()
+        }
+    }
+    
+    func addAnimationView() {
+        let imageView = UIImageView(image: UIImage(named: "hello"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.animationRepeatCount = 1
+        imageView.animationDuration = 3
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let randomSize = 30 + drand48() * 10
+        imageView.frame = CGRect(x: 0, y: 0, width: randomSize, height: randomSize)
+        imageView.alpha = drand48()
+        
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.path = upCurvedPath(imageView: imageView).cgPath
+        animation.duration = 2 + drand48() * 3
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        
+        imageView.layer.add(animation, forKey: nil)
+        
+        self.view.addSubview(imageView)
+    }
+    
+    func upCurvedPath(imageView: UIImageView) -> UIBezierPath {
+        let path = UIBezierPath()
+        
+        let randomX: CGFloat = CGFloat(arc4random_uniform(UInt32(self.view.frame.size.width)))
+        
+        let startPoint = CGPoint(x: randomX, y: self.view.frame.size.height)
+        let endPoint = CGPoint(x: randomX, y: -imageView.frame.size.height)
+        
+        path.move(to: startPoint)
+        
+        let randomXShift = randomX + drand48() * 300
+        let cp1 = CGPoint(x: randomX - randomXShift, y: self.view.frame.size.height / 3)
+        let cp2 = CGPoint(x: randomX + randomXShift, y: (self.view.frame.size.height * 2) / 3)
+        path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        
+        return path
     }
 }
--- FlyEmojiAnimationSample/LottieViewController.swift
+++ FlyEmojiAnimationSample/LottieViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 import Lottie
 
+private let lottieCount: Int = 8
+
 class LottieViewController: UIViewController {
+    var currentCount: Int = 0
+    
     init() {
         super.init(nibName: nil, bundle: nil)
         title = "Lottie"
@@ -27,5 +31,33 @@ class LottieViewController: UIViewController {
     }
     
     @objc func showAnimation() {
+        self.view.subviews.forEach {
+            $0.removeFromSuperview()
+        }
+        
+        self.currentCount = 0
+        
+        (0...lottieCount).forEach { _ in
+            addAnimationView()
+        }
+    }
+    
+    func addAnimationView() {
+        let animationView = AnimationView(animation: Animation.named("lottie_hello")!)
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 3
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let randomX: CGFloat = CGFloat(arc4random_uniform(UInt32(self.view.frame.size.width)))
+        
+        // width를 줄이면 lottie 내부 비율에 맞춰서 height도 줄어듬
+        let width = self.view.frame.size.width/3
+        animationView.frame = CGRect(x: randomX, y: 0, width: width, height: self.view.frame.size.height)
+        
+        animationView.play(fromProgress: 0, toProgress: 1, loopMode: .playOnce)
+        
+        self.view.addSubview(animationView)
+        self.currentCount += 1
     }
 }
